b946a000e05d4eb3fd795238eb7e8ee14a784a70
Anton Shestak <ashestak@icerockdev.com>
2021-09-13 15:52:59 +0700

    buttons with functions and recycler view, viewbinding

--- app/src/main/java/com/example/teachproject/MainActivity.kt
+++ app/src/main/java/com/example/teachproject/MainActivity.kt
@@ -1,37 +1,94 @@
 package com.example.teachproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.teachproject.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var listItems: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val buttonMusic = findViewById<Button>(R.id.button_music)
-        val buttonMap = findViewById<Button>(R.id.button_share)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = viewBinding.root
+        setContentView(view)
 
-        buttonMusic.setOnClickListener(this)
-        buttonMap.setOnClickListener(this)
+        val btnMusic = viewBinding.buttonMusic
+        btnMusic.setOnClickListener(this)
 
+        val btnShare = viewBinding.buttonShare
+        btnShare.setOnClickListener(this)
+
+        val btnChange = viewBinding.buttonChange
+        btnChange.setOnClickListener(this)
+
+        val btnAdd = viewBinding.buttonAdd
+        btnAdd.setOnClickListener(this)
+
+        recyclerView = viewBinding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        listItems = mutableListOf("A", "B", "C", "D", "E", "F", "J", "H")
+        recyclerView.adapter = MyAdapter(listItems)
     }
 
     override fun onClick(p0: View?) {
         when (p0?.id) {
-            R.id.button_music -> {
+            viewBinding.buttonMusic.id -> {
                 val intent = Intent(Intent.ACTION_MAIN)
                 intent.addCategory(Intent.CATEGORY_APP_MUSIC)
                 startActivity(intent)
             }
-            R.id.button_share -> {
+            viewBinding.buttonShare.id -> {
                 val intent = Intent(Intent.ACTION_SEND)
                 intent.type = "text/plain"
                 intent.putExtra(Intent.EXTRA_EMAIL, "test@gmail.com")
                 intent.putExtra(Intent.EXTRA_TEXT, "How are you?")
                 startActivity(intent)
+            }
+            viewBinding.buttonChange.id -> {
+                for (view in listOf(viewBinding.buttonMusic, viewBinding.buttonShare)) {
+                    val borderColor: Int = Color.argb(
+                        255,
+                        Random.nextInt(256),
+                        Random.nextInt(256),
+                        Random.nextInt(256)
+                    )
+                    view.setBackgroundColor(borderColor)
+
+                    val titleColor: Int = Color.argb(
+                        255,
+                        Random.nextInt(256),
+                        Random.nextInt(256),
+                        Random.nextInt(256)
+                    )
+                    view.setTextColor(titleColor)
+
+                    val params = view.layoutParams as MarginLayoutParams
+                    params.setMargins(
+                        Random.nextInt(100),
+                        Random.nextInt(100),
+                        Random.nextInt(100),
+                        Random.nextInt(100)
+                    )
+                    view.requestLayout()
+                }
+            }
+            viewBinding.buttonAdd.id -> {
+                if (viewBinding.editText.text.toString().trim().isNotEmpty()) {
+                    listItems.add(viewBinding.editText.text.toString())
+                    recyclerView.adapter?.notifyDataSetChanged()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/teachproject/MyAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.teachproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val data: List<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+        val textView: TextView = row.findViewById(R.id.number)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_view,
+            parent, false
+        )
+        return MyViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = data[position]
+    }
+
+    override fun getItemCount(): Int = data.size
+
+}
